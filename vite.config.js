52a731eceb4cf137b507abd60e4f9e2473188b03
zainbmaged <zainbmaged114@gmail.com>
2026-07-13 13:36:43 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  base: '/myportfolio/',
 })
